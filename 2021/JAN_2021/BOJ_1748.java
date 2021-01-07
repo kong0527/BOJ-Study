4bfb26ebf2703f296f2f06bb9a21543423915e93
kong0527 <o____ri@naver.com>
2021-01-07 14:05:50 +0900

    add hyperlink

--- 2021/JAN_2021/BOJ_1748.java
+++ 2021/JAN_2021/BOJ_1748.java
@@ -8,27 +8,31 @@ public class BOJ_1748 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         System.out.println(solve(Integer.parseInt(s), s.length()));
-
+    
     }
     public static int solve(int num, int length) {
         if (length == 1) {
             return num;
         }
-
+    
         int ans = 0;
         String flag = "";
         int prev = 0;
-
+    
         for (int i = 1; i < length; i++) {
             flag += "9";
             int tmp = Integer.parseInt(flag);
             ans += (tmp - prev) * i;
             prev = tmp;
         }
-
+    
         int tmp = Integer.parseInt(flag) + 1;
         ans += (num - tmp + 1) * length;
-
+    
         return ans;
     }
 }
+
+
+
+해설은 [여기](https://blog.naver.com/o____ri/222199582486)에 작성하였습니다. 😋
