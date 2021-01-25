0e0566ae0ca8bfc1e7f0c792c27ab604de66dbaf
kong0527 <o____ri@naver.com>
2021-01-25 16:05:04 +0900

    solve Programmers_큰 수 만들기

---
+++ 2021/JAN_2021/Programmers_큰 수 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+        for (int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for (int j = idx; j <= k+i; j++) {
+                if (max < number.charAt(j) - '0') {
+                    idx = j;
+                    max = number.charAt(j) - '0';
+                    idx++;
+                }
+            }
+            answer.append(max);
+        }
+        return answer.toString();
+    }
+}
