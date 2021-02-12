044dd9bc307bf8b21cfb5c51a6fbdbd69f1b8401
kong0527 <o____ri@naver.com>
2021-02-12 22:34:58 +0900

    solve Programmers_전화번호 목록

---
+++ 2021/FEB_2021/Programmers_전화번호 목록.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (phone_book[i].startsWith(phone_book[j])) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
